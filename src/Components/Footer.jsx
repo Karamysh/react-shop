21fc4f26b50420b620599b6c4f9f650de3ed4fcc
Bitvinus <dezenu@yandex.ru>
2022-05-04 14:43:43 +0300

    add: github-pages

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,11 @@ function Footer() {
       <div className="footer-copyright">
         <div className="container">
           © {new Date().getFullYear()} Copyright Text
-          <a className="grey-text text-lighten-4 right" href="#!">
+          <a
+            className="grey-text text-lighten-4 right"
+            href="https://github.com/Bitvinus/react-shop"
+            target="_blank"
+          >
             Repo
           </a>
         </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,12 +4,15 @@ function Header() {
   return (
     <nav className="green darken-1">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
+        <a href="/" className="brand-logo">
           React Shop
         </a>
         <ul id="nav-mobile" className="reight hide-on-med-and-down">
           <li>
-            <a href="!#">Repo</a>
+            <a
+              href="https://bitvinus.github.io/react-shop/"
+              target="_blank"
+            ></a>
           </li>
         </ul>
       </div>
