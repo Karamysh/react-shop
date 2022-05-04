3795a440e6903649f595bbc0b0d9f756a0e003ab
Bitvinus <dezenu@yandex.ru>
2022-05-04 14:19:18 +0300

    Updates

---
+++ precache-manifest.71ac6c052c7682814db7d20ca437c5c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1b556aab2063d562407",
+    "url": "/react-shop/static/css/main.e35e8803.chunk.css"
+  },
+  {
+    "revision": "f1b556aab2063d562407",
+    "url": "/react-shop/static/js/main.f1b556aa.chunk.js"
+  },
+  {
+    "revision": "02c28c6f67659caf852c",
+    "url": "/react-shop/static/js/1.02c28c6f.chunk.js"
+  },
+  {
+    "revision": "4b3be4f6cafc0ba0a031",
+    "url": "/react-shop/static/js/runtime~main.4b3be4f6.js"
+  },
+  {
+    "revision": "3181fe10a9990bff0c26da18c10427a4",
+    "url": "/react-shop/index.html"
+  }
+];
